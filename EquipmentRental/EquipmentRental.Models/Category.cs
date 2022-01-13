fff3e9f16e1fcbb3d26093f52a20981e308519b0
Adriangrz <adriang1412g@gmail.com>
2022-01-13 13:43:15 +0100

    Added category, rent, sportequipment models

---
+++ EquipmentRental/EquipmentRental.Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EquipmentRental.Models
+{
+    public class Category
+    {
+        public Guid CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public ICollection<SportEquipment> SportsEquipment { get; set; }
+    }
+}
---
+++ EquipmentRental/EquipmentRental.Models/SportEquipment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EquipmentRental.Models
+{
+    public class SportEquipment
+    {
+        public Guid SportEquipmentId { get; set; }
+        public string Name { get; set; }
+        public decimal PriceForDay { get; set; }
+        public bool IsAvailable { get; set; }
+        public Guid CategoryId { get; set; }
+        public Category Category { get; set; }
+        public Rent Rent { get; set; }
+    }
+}
